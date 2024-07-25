8d50933740a99e4fac0690bce93c724d5dbb9878
adrianC05 <aacarrion4@espe.edu.ec>
2024-07-25 07:46:39 -0500

    Cambios en el menu de navegacion del Personal

--- lib/Services/plato_service.dart
+++ lib/Services/plato_service.dart
@@ -18,9 +18,16 @@ class PlatoService {
         description: 'Descripción del platillo 2',
       ),
       Plato(
-        name: 'Platillo 2',
+        name: 'Platillo 3',
         imageUrl:
-            'https://unareceta.com/wp-content/uploads/2016/10/pollo-asado.jpg',
+            'https://3.bp.blogspot.com/-0oTrqBs4X6k/VMfN0Soc2zI/AAAAAAAAAEw/XSOp0ZAR36o/s1600/restaurantes-el-palacio-de-la-fritada-guia-enquito-4.jpg',
+        price: 12.0,
+        description: 'Descripción del platillo 2',
+      ),
+      Plato(
+        name: 'Platillo 4',
+        imageUrl:
+            'https://th.bing.com/th/id/OIP.etMozSoAMKlgMpalx8KQhwHaE8?rs=1&pid=ImgDetMain',
         price: 12.0,
         description: 'Descripción del platillo 2',
       ),
