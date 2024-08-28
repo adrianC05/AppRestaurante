b1fabd5a07647e1fe115e3488db1b4443e2a748f
darwin-joel12 <guagchingadarwin14@gmail.com>
2024-08-28 09:20:49 -0500

    Hola

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:proyecto/Views/factura_view.dart';
-import 'package:proyecto/Views/login_view.dart';
 import 'package:proyecto/Views/scanner_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:proyecto/firebase_options.dart';
@@ -25,7 +23,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       debugShowCheckedModeBanner: false,
-      home: LoginScreen(),
+      home: ScannerScreen(),
     );
   }
 }
